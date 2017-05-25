c1d2a35fcb4c8469b9859b725af1166cbc3e5bc8
Yuka.E <yuka.e@emotoyuka-no-iMac.local>
2017-05-25 22:07:31 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(function(){
+   var count = $('.l-mainvisual li').length;
+    
+    var current = 1;
+    
+    var next = 2;
+    
+    var interval = 7000;
+    
+    var duration = 1000;
+    
+    var timer;
+    
+    $('.l-mainvisual li:not(:first-child)').hide();
+    
+    timer = setInterval(slideTimer,interval);
+    function slideTimer(){
+        $('.l-mainvisual li:nth-child(+' + current + ')').fadeOut(duration);
+        $('.l-mainvisual li:nth-child(+' + next + ')').fadeIn(duration);
+        
+        current = next;
+        next = ++next;
+        
+        if(next > count){
+            next = 1;
+        }
+    }
+    
+//    cafemenu
+    
+    $('.contents div[class != "tab1"]').hide();
+    
+    $('.menu-link').click(function(){
+        
+        $('.contents div').hide();
+        
+        $($(this).attr('href')).show();
+        
+        $('.current').removeClass('current');
+        
+        $(this).addClass('current');
+        
+        
+        return false;
+    })
+    
+})
